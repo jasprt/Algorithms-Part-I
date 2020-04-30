d7ba00e74661e142891fba0c74bb9564a3b8e2bd
singh <singh@Jassi-HP>
2020-05-01 01:22:28 +0530

    Import Revision - 1.1

--- Algorithms/src/sort/SelectionSort.java
+++ Algorithms/src/sort/SelectionSort.java
@@ -2,15 +2,15 @@ package sort;
 
 import java.util.Arrays;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class SelectionSort {
 	
 	public static void main(String[] args) {
 		
 		int[] a = {2,5,1,0,5,2,7,9,0,8};
+		StdOut.println(Arrays.toString(a));
 		
-		System.out.println(Arrays.toString(a));
-
-
 	}
 
 }
