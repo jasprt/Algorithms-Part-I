330179f5ccd2a0622c4f32de8b389ff82d739ae9
singh <singh@Jassi-HP>
2020-05-01 01:38:33 +0530

    SelectionSort - Complete Revision 1.3

--- Algorithms/src/sort/SelectionSort.java
+++ Algorithms/src/sort/SelectionSort.java
@@ -1,15 +1,33 @@
 package sort;
 
 import java.util.Arrays;
-
 import edu.princeton.cs.algs4.StdOut;
 
 public class SelectionSort {
 	
+	private static void swap(int[] a, int i, int j) {
+		int temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+	
 	public static void main(String[] args) {
 		
 		int[] a = {2,5,1,0,5,2,7,9,0,8};
-		StdOut.println(Arrays.toString(a));
+		
+		StdOut.println("Unsorted Array: "+Arrays.toString(a));
+		
+		for (int i = 0; i < a.length; i++) {
+			int min = i;
+			for (int j = i+1; j < a.length; j++) {
+				if (a[i] > a[j]) {
+					min = j;
+				}
+			swap(a, min, j);
+			}
+		}
+		
+		
 		
 	}
 
