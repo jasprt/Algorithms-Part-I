636182c6b0ea715847d4cb13f40f829da753b4f9
Jaspreet <Jaspreet@DESKTOP-GLF7KTS>
2020-05-01 00:12:17 +0530

    SelectionSort 1.0

---
+++ Algorithms/src/sort/SelectionSort.java
@@ -0,0 +1,16 @@
+package sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+	
+	public static void main(String[] args) {
+		
+		int[] a = {2,5,1,0,5,2,7,9,0,8};
+		
+		System.out.println(Arrays.toString(a));
+
+
+	}
+
+}
