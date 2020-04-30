e081c3d0aed0864ab584a3021118538dd42aa73c
singh <singh@Jassi-HP>
2020-05-01 01:55:45 +0530

    SelectionSort - Optimize 1.5

--- Algorithms/src/sort/SelectionSort.java
+++ Algorithms/src/sort/SelectionSort.java
@@ -5,30 +5,39 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class SelectionSort {
 	
-	private static void swap(int[] a, int i, int j) {
+	private boolean compare(int a, int b) {
+		if (a > b) {
+			return true;
+		}
+		return false;
+	}
+	
+	private void swap(int[] a, int i, int j) {
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
 	
-	public static void main(String[] args) {
-		
-		int[] a = {2,5,1,0,5,2,7,9,0,8};
-		
-		StdOut.println("Unsorted Array: "+Arrays.toString(a));
+	private void sort(int[] a) {
 		
 		for (int i = 0; i < a.length; i++) {
 			int min = i;
 			for (int j = i+1; j < a.length; j++) {
-				if (a[i] > a[j]) {
+				if (compare(a[min], a[j])) {
 					min = j;
 				}
-			swap(a, min, j);
 			}
+			swap(a, i, min);
 		}
+	}
+	
+	public static void main(String[] args) {
 		
-		
-		
+		int[] a = {2,5,1,0,5,2,7,9,0,8};
+		StdOut.println("Unsorted Array: "+Arrays.toString(a));
+		SelectionSort ss = new SelectionSort();
+		ss.sort(a);
+		StdOut.print("Sorted Array: "+Arrays.toString(a));
 	}
 
 }
