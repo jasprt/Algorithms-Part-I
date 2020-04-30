4fd17b4411008461a0e5d4285f5a3dda99fc018c
singh <singh@Jassi-HP>
2020-05-01 02:21:43 +0530

    Remove unused imports Revision 1.6

--- Algorithms/src/sort/SelectionSort.java
+++ Algorithms/src/sort/SelectionSort.java
@@ -1,10 +1,6 @@
 package sort;
 
 import java.util.Arrays;
-import java.util.Scanner;
-
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
