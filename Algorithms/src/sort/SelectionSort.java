6fa2ab4372aacefb0cfaec60024e145e9d4b3ee0
Jaspreet <Jaspreet@DESKTOP-GLF7KTS>
2020-05-01 08:17:21 +0530

    Compare optimization - Coding standards R: 1.6

--- Algorithms/src/sort/SelectionSort.java
+++ Algorithms/src/sort/SelectionSort.java
@@ -1,47 +1,52 @@
 package sort;
 
 import java.util.Arrays;
+
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class SelectionSort {
-	
-	private boolean compare(int a, int b) {
-		if (a > b) {
-			return true;
+
+	private boolean compare(int a, int b, char c) {
+
+		switch (c) {
+		case 'A': 	return a > b;
+		case 'D': 	return a < b;
+		default:	throw new IllegalArgumentException("Either A or D char allowed for ascending and descending order sort respectively");
 		}
-		return false;
 	}
-	
+
 	private void swap(Integer[] a, int i, int j) {
 		int temp = a[i];
 		a[i] = a[j];
 		a[j] = temp;
 	}
-	
-	private void sort(Integer[] a) {
-		
+
+	private void sort(Integer[] a, char c) {
+
 		for (int i = 0; i < a.length; i++) {
 			int min = i;
 			for (int j = i+1; j < a.length; j++) {
-				if (compare(a[min], a[j])) {
+				if (compare(a[min], a[j] , c)) {
 					min = j;
 				}
 			}
 			swap(a, i, min);
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Integer[] a = new Integer[10];
 		for (int i = 0; i < a.length; i++) {
 			a[i] = StdRandom.uniform(10);
 		}
 		StdOut.println("Random Unsorted Array: "+Arrays.toString(a));
 		SelectionSort ss = new SelectionSort();
-		ss.sort(a);
-		StdOut.print("Sorted Array: "+Arrays.toString(a));
+		ss.sort(a, 'A');
+		StdOut.println("Sorted Array in Asc: "+Arrays.toString(a));
+		ss.sort(a, 'D');
+		StdOut.println("Sorted Array in Desc: "+Arrays.toString(a));
 	}
 
 }
